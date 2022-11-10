5dfea7f7a2097634a35efccfd85114cc7345a37a
Pedra96 <darkraidialga@yahoo.it>
2022-11-10 17:20:58 +0100

    completato esercizio

--- js/main.js
+++ js/main.js
@@ -5,14 +5,20 @@ let sconto20Percento=20;
 
 let sconto40Percento=40;
 //
+let nome=document.getElementById("valoreNome");
+let cognome=document.getElementById("valoreCognome");
 
 let kmDaPercorrere=document.getElementById("kilometri");
 let eta=document.getElementById("età");
 
-document.getElementById("bottone").onclick = etaCheck;
-function check(){
-    document.getElementById("biglietto").innerHTML=kmDaPercorrere.value;
-    console.log(kmDaPercorrere);
+document.getElementById("bottone").onclick =generaBiglietto;
+function generaBiglietto(){
+    document.getElementById("Nomebiglietto").innerHTML="Nome: "+nome.value;
+    document.getElementById("Cognomebiglietto").innerHTML="Cognome: "+cognome.value;
+    document.getElementById("etàbiglietto").innerHTML="Età: "+eta.value;
+
+    
+    etaCheck();
 }
 function percentage(num, per)
 {
@@ -24,11 +30,11 @@ function etaCheck(){
     let checkEta = eta.value;
 if(checkEta<18){
     let sconto =percentage(prezzoTotaleBiglietto,sconto20Percento).toFixed(2);
-    document.getElementById("biglietto").innerHTML=(prezzoTotaleBiglietto-sconto).toFixed(2);
-}else if(checkEta>65){
+    document.getElementById("Prezzobiglietto").innerHTML="Prezzo: "+(prezzoTotaleBiglietto-sconto).toFixed(2);
+}else if(checkEta>=65){
     let sconto =percentage(prezzoTotaleBiglietto,sconto20Percento).toFixed(2);
-    document.getElementById("biglietto").innerHTML=(prezzoTotaleBiglietto-sconto).toFixed(2);
+    document.getElementById("Prezzobiglietto").innerHTML="Prezzo: "+(prezzoTotaleBiglietto-sconto).toFixed(2);
 }else{
-    document.getElementById("biglietto").innerHTML= prezzoTotaleBiglietto.toFixed(2);
+    document.getElementById("Prezzobiglietto").innerHTML="Prezzo: "+ prezzoTotaleBiglietto.toFixed(2);
 }
 }
